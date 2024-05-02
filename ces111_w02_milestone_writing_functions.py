13b657b96857579191eb2fa859f5cdb0499db6c9
serdotte <rodrigo.serdotte@gmail.com>
2024-05-01 21:35:15 -0300

    Atualization

---
+++ ces111_w02_milestone_writing_functions.py
@@ -0,0 +1,11 @@
+import random
+
+#a determiner (sometimes known as an article)
+#a noun
+#a verb
+# Create a list of strings and assign
+# the list to a variable named words.
+words = ["boy", "girl", "cat", "dog", "bird", "house"]
+
+
+
