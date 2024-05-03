aec4b3cb8c538e915419a8beb0126095973bec42
serdotte <rodrigo.serdotte@gmail.com>
2024-05-03 19:17:01 -0300

    Milestone sentences added

--- rascunho.py
+++ rascunho.py
@@ -1,49 +1,25 @@
-# practing and testing
-import requests
-from bs4 import BeautifulSoup
-import math
-from datetime import datetime
+import random
 
-print("****************************************************")
-print("This program calculates and reports the tire volume.")
-print("****************************************************")
-print("")
+def main():
+    sentence = make_sentence()
+    print(sentence)
 
-current_date_and_time = datetime.now(tz=None)
-formatted_date = current_date_and_time.strftime("%Y-%m-%d")
-print(formatted_date)
+def make_sentence():
+    determiner = get_determiner()
+    noun = get_noun()
+    verb = get_verb()
+    return f"{determiner} {noun} {verb}"
 
+def get_determiner():
+    determiners = ["the", "a", "an"]
+    return random.choice(determiners)
 
-while True:
-    width = int(input("Enter the width of the tire in mm (ex 205): "))
-    ratio = int(input("Enter the aspect ratio of the tire (ex 60): "))
-    diameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
+def get_noun():
+    nouns = ["cat", "dog", "bird"]
+    return random.choice(nouns)
 
-    user_tire_dates = [width, ratio, diameter]
-    
-    # Concatenando os dados para formar a URL de pesquisa
-    search_url = f"https://www.gpneus.com/pneus/?search={width} {ratio} {diameter}"
-    
-    # Enviando a solicitação HTTP para a URL de pesquisa
-    response = requests.get(search_url)
-    
-    if response.status_code == 200:
-        # Analisando o conteúdo HTML da página
-        soup = BeautifulSoup(response.content, "html.parser")
-        
-        # Encontrando o elemento que contém o preço
-        price_element = soup.find("span", class_="price")
-        
-        if price_element:
-            price_text = price_element.get_text(strip=True)
-            price_tire = float(price_text.replace("R$", "").replace(",", "."))
-            
-            print(f"The price for the tire is {price_tire:.2f} BRL")
-        else:
-            print("Price not found on the website.")
-    else:
-        print("Failed to retrieve data from the website.")
-    
-    repeat = input("Do you want to calculate another tire volume? (yes/no): ")
-    if repeat.lower() != "yes":
-        break
+def get_verb():
+    verbs = ["runs", "jumps", "flies"]
+    return random.choice(verbs).capitalize()
+
+main()
