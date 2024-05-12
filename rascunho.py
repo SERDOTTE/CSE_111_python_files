84e5fba4422244276bfeb599eff7903c7546833c
serdotte <rodrigo.serdotte@gmail.com>
2024-05-11 23:00:17 -0300

    updated

--- rascunho.py
+++ rascunho.py
@@ -1,25 +1,13 @@
-import random
+# Example 4
 
-def main():
-    sentence = make_sentence()
-    print(sentence)
+    # The variables e and f can be any floating-
+    # point numbers from any calculation.
+e = 7.135
+f = 7.128
 
-def make_sentence():
-    determiner = get_determiner()
-    noun = get_noun()
-    verb = get_verb()
-    return f"{determiner} {noun} {verb}"
-
-def get_determiner():
-    determiners = ["the", "a", "an"]
-    return random.choice(determiners)
-
-def get_noun():
-    nouns = ["cat", "dog", "bird"]
-    return random.choice(nouns)
-
-def get_verb():
-    verbs = ["runs", "jumps", "flies"]
-    return random.choice(verbs).capitalize()
-
-main()
+if abs(e - f) < 0.01:
+        print(f"{e} and {f} are close enough so")
+        print("we'll consider them to be equal.")
+else:
+        print(f"{e} and {f} are not close and")
+        print("therefor not equal.")
